f29d81eab6f4bb865a750d9a2f8c8810b8e5a95f
Mihail Filimon <mih.fil76@gmail.com>
2025-03-25 14:20:00 +0200

    Update

--- app.vue
+++ app.vue
@@ -19,13 +19,16 @@ useHead({
 </script>
 
 <template>
+  <header>
+    <meta content="9tld3YqUQD16DMf3vcY349WLkMdRyFcpL_PAW36RwNs" name="google-site-verification"/>
+  </header>
   <UApp>
     <noscript>
       <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-K6SQMZ8S"
           height="0"
-          width="0"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-K6SQMZ8S"
           style="display:none;visibility:hidden"
+          width="0"
       ></iframe>
     </noscript>
     <AppHeader/>
